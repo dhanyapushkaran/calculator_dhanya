2fb5fd7181cf02a770f92a30a94c1edf2f0cd5d5
Dhanya <dhanyapushkaran@gmail.com>
2021-07-03 15:36:17 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,4 @@
+let displayNumber=(num)=>inpt.value+=num;
+let evaluateExpression=()=>inpt.value=eval(inpt.value)
+let clearBox=()=>inpt.value="";
+let DeleteNumber=()=>inpt.value=inpt.value.slice(0,-1)
