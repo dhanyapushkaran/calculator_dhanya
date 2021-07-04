8f88880ca00cbf3cb8b9ed8369643be8fc69c441
Dhanya <dhanyapushkaran@gmail.com>
2021-07-04 20:06:14 +0530

    edited calc

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let displayNumber=(num)=>inpt.value+=num;
 let evaluateExpression=()=>inpt.value=eval(inpt.value)
 let clearBox=()=>inpt.value="";
 let DeleteNumber=()=>inpt.value=inpt.value.slice(0,-1)
+function evaluateEqual(){
+        let temp=inpt.value
+       let res= eval(temp)
+       inpt.value=` ${temp} = ${res}`
+    }
